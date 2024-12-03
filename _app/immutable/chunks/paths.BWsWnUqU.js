0efeae7ef696854178a6adc41c53836ee3147c55
Wesley7104 <Wesley7104@gmail.com>
2024-12-03 04:44:28 +0000

    Deploying to gh-pages from @ Wesley7104/Wesley7104.github.io@8e07cdd36c726caed22ebcb7cc2afaa6cff03551 🚀

---
+++ _app/immutable/chunks/paths.BWsWnUqU.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_18uj9ci)==null?void 0:s.base)??"/wesley7104.github.io";var e;const a=((e=globalThis.__sveltekit_18uj9ci)==null?void 0:e.assets)??t;export{a,t as b};
