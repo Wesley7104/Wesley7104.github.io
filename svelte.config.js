e809165fa1305359adfaac98bd5a269870d9571c
wesley7104 <wesley7104@gmail.com>
2024-12-02 19:16:17 -0600

    fixed config.kit.adapter

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,13 @@ const config = {
 
 	kit: {
 		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
 			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
-		pages: 'build/'
 	}
 };
 
