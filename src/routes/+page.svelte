d2dbfdd9ce92b0ea857e8e541905a4969c5a3b6a
wesley7104 <wesley7104@gmail.com>
2024-12-13 00:14:51 -0600

    Added more to the timeline.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -368,6 +368,63 @@ onMount(() => {
 
               </div>
               <hr />
+              </li>
+              <li>
+                <hr />
+                <div class="timeline-middle">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="timeline-start md:text-end mb-10">
+                  <time class="font-mono italic">October 2021</time>
+                  <div class="text-lg font-black">Beach For House Website</div>
+                  <div class="text-base">Built a website for Thomas Beach for his run for South Carolina House or Representatives. Integrated his campaign and donations links and appliactions to allow online giving.</div>
+                </div>
+                <hr />
+              </li>
+              <li>
+                <hr />
+                <div class="timeline-middle">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="timeline-end mb-10">
+                    <time class="font-mono italic">December 2021</time>
+                    <div class="text-lg font-black">NASA Image of the Day Viewing App</div>
+                    <div class="text-base">TBuilt a simple app using NASA's API to view the daily image of the day. This application was built using Svelte.js and SvelteKit, it was a real turning point in my web development journey.
+                    </div>
+                </div>
+                <hr />
+            </li>
+            <li>
+              <hr />
+              <div class="timeline-middle">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                  </svg>
+              </div>
+              <div class="timeline-start md:text-end mb-10">
+                <time class="font-mono italic">2022</time>
+                <div class="text-lg font-black">The Year I Discovered Blockchain and Web3</div>
+                <div class="text-base">I started learning about blockchain and web3 in 2022. I was fascinated by the potential of the technology and the ability to create decentralized applications. I started learning about Solidity and Ethereum and quickly realized that this was something I wanted to be a part of. I started learning about the different blockchains and the different use cases for each one. I spent a lot of time in the DeFi space, and discovered a whole new passion. Trading and Crypto!</div>
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div class="timeline-middle">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                  </svg>
+              </div>
+              <div class="timeline-end mb-10">
+                  <time class="font-mono italic">October 2022</time>
+                  <div class="text-lg font-black">Trading Bots Consumed My Life</div>
+                  <div class="text-base">I started building trading bots in 2022. I was fascinated by the potential of the technology and the ability to create decentralized applications. Building out automated trading strategies using TradingView and PineScript, to automate the backtesting and eventually the trading. I slowly lost interest in Web3 and started solidifying my skills in trading.</div>
+              </div>
+              <hr />
           </li>
           <li>
             <hr />
@@ -377,27 +434,12 @@ onMount(() => {
                 </svg>
             </div>
             <div class="timeline-start md:text-end mb-10">
-              <time class="font-mono italic">October 2021</time>
-              <div class="text-lg font-black">Beach For House Website</div>
-              <div class="text-base">Built a website for Thomas Beach for his run for South Carolina House or Representatives. Integrated his campaign and donations links and appliactions to allow online giving.</div>
+              <time class="font-mono italic">February 2023</time>
+              <div class="text-lg font-black">Trading Became My Passion</div>
+              <div class="text-base">Pivoting from trading bots to manual trading changed my outlook on the markets. I started to notice patterns and this led me to start trading on my own. I became a member of the WolfPack Trade Group, this iswhere i learned to trade using data and statistics. Changing my trading strategy to a more data-driven approach was a game changer for me. I started to see the markets in a whole new way and I was able to make much better decisions along with implementing risk managment strategies to protect capital as well.</div>
             </div>
             <hr />
           </li>
-          <li>
-            <hr />
-            <div class="timeline-middle">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
-                </svg>
-            </div>
-            <div class="timeline-end mb-10">
-                <time class="font-mono italic">December 2021</time>
-                <div class="text-lg font-black">NASA Image of the Day Viewing App</div>
-                <div class="text-base">TBuilt a simple app using NASA's API to view the daily image of the day. This application was built using Svelte.js and SvelteKit, it was a real turning point in my web development journey.
-                </div>
-            </div>
-            <hr />
-        </li>
         </ul>
     </div>
 
